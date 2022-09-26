329aed318fefea85e5a28ec7fef841ce4345f6a4
polina belyakovich <polina.belyakovich@inbox.ru>
2022-09-26 22:55:42 +0400

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        char MyChar = '\u00C1';
+        Character My = '\u20001';
+        System.out.println(MyChar);
+        System.out.println(My);
+    }
+}
